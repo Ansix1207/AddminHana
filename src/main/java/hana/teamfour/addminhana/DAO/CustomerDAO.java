7d5d9c281c978c241100f8583788d1b2063df49f
ChaedongIm <chaedong.im.dev@gmail.com>
2023-06-20 17:14:15 +0900

    Update: CustomerDAO 에러 밸리데이션 추가

--- src/main/java/hana/teamfour/addminhana/DAO/CustomerDAO.java
+++ src/main/java/hana/teamfour/addminhana/DAO/CustomerDAO.java
@@ -31,21 +31,22 @@ public class CustomerDAO {
             String query = "select * from customer where c_id = ? ";
             System.out.println("query = " + query);
             pstmt = conn.prepareStatement(query);
-            pstmt.setInt(1, 1);
+            pstmt.setInt(1, _c_id);
             ResultSet rs = pstmt.executeQuery();
             System.out.println("rs 전");
 
-            Integer c_id = rs.getInt("c_id");
-            String c_name = rs.getString("c_name");
-            String c_rrn = rs.getString("c_rrn");
-            Character c_gender = rs.getString("c_gender").charAt(0);
-            String c_address = rs.getString("c_address");
-            String c_mobile = rs.getString("c_mobile");
-            String c_job = rs.getString("c_job");
-            String c_description = rs.getString("c_description");
-            Integer e_id = rs.getInt("e_id");
-
-            customerEntity = new CustomerEntity(c_id, c_name, c_rrn, c_gender, c_address, c_mobile, c_job, c_description, e_id);
+            if (rs.next()) {
+                Integer c_id = rs.getInt("c_id");
+                String c_name = rs.getString("c_name");
+                String c_rrn = rs.getString("c_rrn");
+                Character c_gender = rs.getString("c_gender").charAt(0);
+                String c_address = rs.getString("c_address");
+                String c_mobile = rs.getString("c_mobile");
+                String c_job = rs.getString("c_job");
+                String c_description = rs.getString("c_description");
+                Integer e_id = rs.getInt("e_id");
+                customerEntity = new CustomerEntity(c_id, c_name, c_rrn, c_gender, c_address, c_mobile, c_job, c_description, e_id);
+            }
             rs.close();
             pstmt.close();
             conn.close();
