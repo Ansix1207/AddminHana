d779321de0db1bdb4fbdddcaf50c521fdc99d6fb
Nam sunghee <118495934+tusker4@users.noreply.github.com>
2023-06-21 14:22:36 +0900

    Feat : 금융상품조회, 상품가입 페이지 1차구현 (#21)
    * Feat: 테스트용
    
    * Rename: loan_join.jsp 작성, index.jsp ì에 연결,  newpage.html 삭제
    
    * Feat: 금융상품조회페이지 생성
    
    * Feat : 하나font 파일 생성
    
    * Chore : nav작성, nav 방향전올바르게 수정
    
    * Chore: loan_join.jsp css수정, 크기맞춤
    
    * Chore : input 박스안의 글씨크기조정
    
    * Feat: 금융상품조회페이지 ãCSS 잓작성
    
    * Feat: 금융상품조회 페이지 DB연결, 페이지이름 재정의
    
    * Chore: 금융상품조회 페이지 혼자 연습했던 내용 제거
    
    * Fix : 금융상품페이지 review 내용을 반영하여 수정
    
    * Chore : loan_join 페이지의 <%=contextPath%> 으로 수정

---
+++ src/main/java/hana/teamfour/addminhana/DAO/LoanProductDAO.java
@@ -0,0 +1,54 @@
+package hana.teamfour.addminhana.DAO;
+
+import hana.teamfour.addminhana.entity.ProductEntity;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+public class LoanProductDAO {
+    Connection conn = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+    // 이 코드는 LoanProductDAO 클래스의 멤버 변수로 데이터베이스 연결과 관련된 객체들을 선언하고, 초기값으로 null을 설정합니다. 
+
+    public static Connection getConnection() throws Exception {
+        Class.forName("oracle.jdbc.OracleDriver");
+        Connection con = DriverManager.getConnection
+                ("jdbc:oracle:thin:@//localhost:1521/xe", "DB1", "1234");
+        return con;
+    }
+
+
+    public ArrayList<ProductEntity> getLoanProductList() {
+        ArrayList<ProductEntity> productEntityList = new ArrayList<>();
+
+        try {
+            conn = getConnection();
+
+            String sql = "select p_name, p_limit, p_interestrate from product where p_category in (?, ?)";
+
+            ps = conn.prepareStatement(sql);
+            ps.setString(1, "신용대출");
+            ps.setString(2, "담보대출");
+            rs = ps.executeQuery();
+
+            while (rs.next()) {
+                ProductEntity productEntity = new ProductEntity();
+                productEntity.setP_name(rs.getString(1));
+                productEntity.setP_limit(rs.getInt(2));
+                productEntity.setP_interestrate(rs.getDouble(3));
+                productEntityList.add(productEntity);
+            }
+
+            conn.close();
+            ps.close();
+            rs.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return productEntityList;
+    }
+}
---
+++ src/main/java/hana/teamfour/addminhana/service/LoanProductService.java
@@ -0,0 +1,18 @@
+package hana.teamfour.addminhana.service;
+
+import hana.teamfour.addminhana.DAO.LoanProductDAO;
+import hana.teamfour.addminhana.entity.ProductEntity;
+
+import java.util.ArrayList;
+
+public class LoanProductService {
+    private final LoanProductDAO loanProductDao;
+
+    public LoanProductService(LoanProductDAO loanProductDao) {
+        this.loanProductDao = loanProductDao;
+    }
+
+    public ArrayList<ProductEntity> getLoanProductList() {
+        return loanProductDao.getLoanProductList();
+    }
+}
