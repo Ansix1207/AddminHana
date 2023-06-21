4f430e24de06aaee5f85c33a987689811be569fc
parkyolo <39394642+parkyolo@users.noreply.github.com>
2023-06-21 16:55:37 +0900

    Feat: 계좌 현황 페이지 1차 구현 (#28)
    * feat: 대출 현황 페이지 생성
    
    * Feat: navbar 주석 제거, card css margin 수정
    
    * Rename: 계좌 현황 css 파일명 변경
    
    * Feat: 서블릿 연결, url-pattern에 .do 추가
    
    * Feat: DB에서 대출 계좌 정보 가져와서 view에 전달하기
    
    * Rename: 계좌 현황 jsp 파일명 변경
    
    * Chore: web.xml 변경사항 되돌리기
    
    * Feat: 예금/적금/대출 현황 페이지 링크 추가
    
    * Feat: 대출 계좌 정보 가져오기 DAO, Service 수정
    
    * Feat: 대출 자산 가져오기 DAO, Service, 객체 수정
    
    * Refactor: AssetInfo 자료형 배열 -> 객체로 변경
    
    * Chore: mainController 파ì.java¼ 초기화
    
    * Refactor: entity í파ì파ìclass 통일
    
    * Rename: 계좌 정보 조회 DAO, Service 파일명 변경
    
    * Refactor: ëEn변경된 Entity class명으로 자경로 수정
    
    * Feat: Button, get 요청을청을 지정한 버튼으로 변경
    
    * Feat: 대출 자산 비율 계산
    
    * Feat: 대출 자산 비율 계산
    
    * Chore: MainController 초기화
    
    * Feat: 계좌, 자산 정보 불러오기
    
    * Rename: 대출 현황 controller명 변경
    
    * Feat: 예금 현황 controller, dao, service 생성
    
    * Feat: 적금 현황 controller, dao, servie 생성
    
    * Feat: 자산 차트 추가
    
    * Rename: 대출 현황 파일명 변경
    
    * Feat: css 수정, 빈 도넛 차트 그리기
    
    * Refactor: class명 camel case로 변경
    
    * Fix: index.jsp 삭제
    
    * Fix: 계좌 잔액 보여주는 방식 변경
    
    * Style: main tag padding 추가

---
+++ src/main/java/hana/teamfour/addminhana/controller/CurDepositInfoController.java
@@ -0,0 +1,57 @@
+package hana.teamfour.addminhana.controller;
+
+import hana.teamfour.addminhana.DAO.DepositAssetDAO;
+import hana.teamfour.addminhana.DAO.DepositAccountDAO;
+import hana.teamfour.addminhana.entity.AccountEntity;
+import hana.teamfour.addminhana.entity.AssetEntity;
+import hana.teamfour.addminhana.service.DepositAssetService;
+import hana.teamfour.addminhana.service.DepositAccountService;
+import hana.teamfour.addminhana.service.DepositBalanceService;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+@WebServlet("/depositInfo")
+public class CurDepositInfoController extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        requestPro(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        requestPro(request, response);
+    }
+
+
+    protected void requestPro(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        DepositAccountDAO depositAccountDAO = new DepositAccountDAO();
+        DepositAssetDAO depositAssetDAO = new DepositAssetDAO();
+
+        DepositAccountService depositAccountService = new DepositAccountService(depositAccountDAO);
+        DepositAssetService depositAssetService = new DepositAssetService(depositAssetDAO);
+        DepositBalanceService depositBalanceService = new DepositBalanceService(depositAccountDAO);
+
+        AssetEntity assetEntity = depositAssetService.getDepositAsset();
+        ArrayList<AccountEntity> accountEntity = depositAccountService.getDepositInfoList();
+        Integer[] depositBalance = depositBalanceService.getDepositBalance();
+
+        request.setAttribute("accountEntity", accountEntity);
+        request.setAttribute("assetEntity", assetEntity);
+        request.setAttribute("depositBalance", depositBalance);
+        request.setAttribute("productType", "예금");
+
+        String site = "views/sessionOnAccInfo.jsp";
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher(site);
+        dispatcher.forward(request, response);
+    }
+}
