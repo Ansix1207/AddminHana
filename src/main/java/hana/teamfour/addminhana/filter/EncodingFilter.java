58d518138c1cf3a72c2e4db187e2f76aa4896793
ChaedongIm <chaedong.im.dev@gmail.com>
2023-06-22 14:05:36 +0900

    Refactor: 불필요한 주석 제거

--- src/main/java/hana/teamfour/addminhana/filter/EncodingFilter.java
+++ src/main/java/hana/teamfour/addminhana/filter/EncodingFilter.java
@@ -11,10 +11,7 @@ public class EncodingFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
-        // Pre-processing code goes here
         request.setCharacterEncoding("utf-8");
-        // Pass the request and response to the next filter or servlet in the chain
         chain.doFilter(request, response);
-        // Post-processing code goes here
     }
 }
