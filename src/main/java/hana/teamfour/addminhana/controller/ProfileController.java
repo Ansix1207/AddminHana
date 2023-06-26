58429ab1c15b47e7dcb9dfb3f57e74eaf5bfea7e
ChaedongIm <chaedong.im.dev@gmail.com>
2023-06-26 17:28:34 +0900

    Update: 메인 페이지 -> 주민번호 로그인 추가

--- src/main/java/hana/teamfour/addminhana/controller/ProfileController.java
+++ src/main/java/hana/teamfour/addminhana/controller/ProfileController.java
@@ -11,6 +11,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/profile/*")
@@ -35,32 +36,40 @@ public class ProfileController extends HttpServlet {
     }
 
     private void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
         response.setContentType("text/html;charset=utf-8");
         String nextPage = "/views/profile.jsp";
         String action = request.getParameter("action");
         String description = request.getParameter("descriptionText");
-
+        String customerRRN = (String) request.getParameter("customerRRN");
         try {
             // 1. 로그인 후 /profile 의 경로로 컨트롤러를 타고 들어오면
             // 1.1. CustomerSummaryDTO 를 서비스단에서 가져와서 request에 setAttribute
-            CustomerSummaryDTO customerSummaryDTO = customerService.getCustomerSummaryDTOById(1);
-            request.setAttribute("customerSummaryDTO", customerSummaryDTO);
-
-            // 2. AssetSummary
-            // 3. description
-            if (action != null && action.equals("description")) {
-                customerSummaryDTO.setC_description(description);
-                boolean hasUpdated = customerService.updateCustomerDescription(customerSummaryDTO);
-                // ! false 일 떄 토스트 뜨지 않게 테스트중 
-                // TODO: profile 페이지에서 새로고침했을 때 Toast 뜨지 않도록 변경해야함
-                // TODO: 새로고침했을 때 attribute를 삭제해줘야 하는 방법이 뭘까?
-                // TODO: Toast를 한번 띄우고 attribute 삭제 콜을 해야할것 같기도 하고.. ? 
-                request.setAttribute("hasUpdatedDescription", hasUpdated);
+            CustomerSummaryDTO customerSummaryDTO = customerService.getCustomerSummaryDTOByRrn(customerRRN);
+            System.out.println("customerSummaryDTO controller = " + customerSummaryDTO);
+            if (customerSummaryDTO == null) {
+                nextPage = "/views/main.jsp";
+                RequestDispatcher dispatcher = request.getRequestDispatcher(nextPage);
+                dispatcher.forward(request, response);
+            } else {
+                session.setAttribute("userSession", customerSummaryDTO);
+                request.setAttribute("customerSummaryDTO", customerSummaryDTO);
+                // 2. AssetSummary
+                // 3. description
+                if (action != null && action.equals("description")) {
+                    customerSummaryDTO.setC_description(description);
+                    boolean hasUpdated = customerService.updateCustomerDescription(customerSummaryDTO);
+                    // ! false 일 떄 토스트 뜨지 않게 테스트중 
+                    // TODO: profile 페이지에서 새로고침했을 때 Toast 뜨지 않도록 변경해야함
+                    // TODO: 새로고침했을 때 attribute를 삭제해줘야 하는 방법이 뭘까 ?
+                    // TODO: Toast를 한번 띄우고 attribute 삭제 콜을 해야할것 같기도 하고.. ?
+                    request.setAttribute("hasUpdatedDescription", hasUpdated);
+                }
+                // 4. recommendation
+                // 5. calendar data
+                RequestDispatcher dispatcher = request.getRequestDispatcher(nextPage);
+                dispatcher.forward(request, response);
             }
-            // 4. recommendation
-            // 5. calendar data
-            RequestDispatcher dispatcher = request.getRequestDispatcher(nextPage);
-            dispatcher.forward(request, response);
         } catch (Exception e) {
             e.printStackTrace();
         }
