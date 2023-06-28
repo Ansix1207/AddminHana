8f44811843a968788ae94b6f36bdaf70ee2df8b2
helloalpaca <jms393497@gmail.com>
2023-06-28 10:00:06 +0900

    Feature: make EmployeeDTO, EmployeeService

---
+++ src/main/java/hana/teamfour/addminhana/service/EmployeeService.java
@@ -0,0 +1,15 @@
+package hana.teamfour.addminhana.service;
+
+import hana.teamfour.addminhana.DAO.EmployeeDAO;
+import hana.teamfour.addminhana.DTO.EmployeeDTO;
+import hana.teamfour.addminhana.entity.EmployeeEntity;
+
+public class EmployeeService {
+
+    public EmployeeDTO login(String id, String pw) {
+        EmployeeDAO employeeDAO = EmployeeDAO.getInstance(); //싱글톤 패턴
+        EmployeeEntity employeeEntity = employeeDAO.login(id, pw);
+        EmployeeDTO employeeDTO = new EmployeeDTO(employeeEntity);
+        return employeeDTO;
+    }
+}
