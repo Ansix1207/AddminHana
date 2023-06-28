847e4c3af11cb58e7a2c87f801702a87cf190e8d
helloalpaca <jms393497@gmail.com>
2023-06-28 10:04:20 +0900

    Fix: apply try-with-resources, design patterns, singleton

--- src/main/java/hana/teamfour/addminhana/service/EmployeeService.java
+++ src/main/java/hana/teamfour/addminhana/service/EmployeeService.java
@@ -9,7 +9,10 @@ public class EmployeeService {
     public EmployeeDTO login(String id, String pw) {
         EmployeeDAO employeeDAO = EmployeeDAO.getInstance(); //싱글톤 패턴
         EmployeeEntity employeeEntity = employeeDAO.login(id, pw);
-        EmployeeDTO employeeDTO = new EmployeeDTO(employeeEntity);
+        EmployeeDTO employeeDTO = null;
+        if (employeeEntity != null) {
+            employeeDTO = new EmployeeDTO(employeeEntity);
+        }
         return employeeDTO;
     }
 }
