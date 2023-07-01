cfcffe7b353414d3e001431ce353f1e9a4eb44ba
ChaedongIm <chaedong.im.dev@gmail.com>
2023-07-01 15:26:23 +0900

    Refactor: AssetSetterDTO 활용

---
+++ src/main/java/hana/teamfour/addminhana/DTO/AssetSetterDTO.java
@@ -0,0 +1,15 @@
+package hana.teamfour.addminhana.DTO;
+
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class AssetSetterDTO {
+    private Integer c_id;
+    private String category;
+    private String assetDTOName;
+    private String accountListName;
+}
